716ab319f343d26add7dd733102064007f8631ef
Aleksander <57395146+AleksanderEZ@users.noreply.github.com>
2021-06-14 13:41:16 +0100

    StateStack

--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -3,5 +3,5 @@ Event = require "lib.knife.event"
 push = require "lib.push"
 Timer = require "lib.knife.timer"
 
-require 'src/constants'
-require 'src/states/StateStack'
+require 'src.constants'
+require 'src.states.StateStack'
