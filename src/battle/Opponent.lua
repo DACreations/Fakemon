ae955372b301fd2c4e23f1375f71710ed9e3ebe3
Aleksander <57395146+AleksanderEZ@users.noreply.github.com>
2021-06-17 21:39:20 +0100

    Battles finished

--- src/battle/Opponent.lua
+++ src/battle/Opponent.lua
@@ -4,6 +4,4 @@ function Opponent:init(def)
     self.party = def.party
 end
 
-function Opponent:takeTurn()
-
-end
+function Opponent:takeTurn() end
--- src/entity/Player.lua
+++ src/entity/Player.lua
@@ -2,4 +2,10 @@ Player = Class{__includes = Entity}
 
 function Player:init(def)
     Entity.init(self, def)
+
+    self.party = Party {
+        pokemon = {
+            Fakemon(Fakemon.getRandomDef(), 5)
+        }
+    }
 end
