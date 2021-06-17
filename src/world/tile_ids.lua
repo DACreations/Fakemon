1650119ad33886ffda381b404cc5954c549348c1
Dario-ESAT <darioad3000@gmail.com>
2021-06-17 21:56:07 +0200

    rocks

--- src/world/tile_ids.lua
+++ src/world/tile_ids.lua
@@ -2,5 +2,7 @@ TILE_IDS = {
     ['grass'] = {37},
     ['empty'] = 31,
     ['tall-grass'] = 30,
-    ['half-tall-grass'] = 28
+    ['half-tall-grass'] = 28,
+    ['rocks'] = {20}
+    -- ['rocks'] = {1,2,3,10,11,12,19,20,21}
 }
